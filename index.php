6af0dbf44ac128f5084632c4f751b220f28a1f3a
Sven-Koene <32331946+Sven-Koene@users.noreply.github.com>
2017-12-13 12:34:25 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,59 @@
+<?php
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" type="text/css" href="normalize.css">
+    <link rel="stylesheet" type="text/css" href="kinderopvang.css">
+
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <div class="header">
+        <h1>Babysit service</h1>
+    </div>
+    <div class="content">
+        <div class="sizeright">
+            <div class="block">
+                <div><a href="reserveer.php">Check beschikbaarheid en meld je kind aan.</a></div>
+            </div>
+            <div class="block">
+                <div>Foto's en Video's</div>
+            </div>
+        </div>
+
+        <div class="sizeleft"></div>
+            <div class="indelingPic">
+                <div>Dagindeling</div>
+            </div>
+            <div class="block">
+                <div>Visie</div>
+            </div>
+        </div>
+        <br>
+
+
+
+
+
+        <div class="onderzoek">
+            <div>onderzoek 1</div>
+        </div>
+
+        <div class="onderzoek">
+            <div>onderzoek 2</div>
+        </div>
+
+        <div class="onderzoek">
+            <div>onderzoek 3</div>
+        </div>
+
+        <footer>woorden</footer>
+    </div>
+
+</body>
+</html>
